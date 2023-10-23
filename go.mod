7671a57145e862b89aa03614502628893041d320
Laksa Bayu <laksabayu18.com>
2023-10-23 21:10:42 +0700

    first commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/trieaji/app-say-hello
+
+go 1.21.3
+
+require github.com/trieaji/go-say-hello v1.0.0 // indirect
---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+import(
+	"fmt"
+	go_say_hello "github.com/trieaji/go-say-hello"
+) 
+
+func main() {
+	fmt.Println(go_say_hello.sayHello())
+}
